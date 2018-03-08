08860edd099af6cbcf6755647eef3466eefc50dd
PoojaGaonkar <pooja.r.gaonkar@gmail.com>
2018-03-08 13:20:12 +0530

    Added toolbar

--- AWEstimates/app/src/main/java/adweb/com/awteamestimates/IssueSummaryActivity.java
+++ AWEstimates/app/src/main/java/adweb/com/awteamestimates/IssueSummaryActivity.java
@@ -1,12 +1,18 @@
 package adweb.com.awteamestimates;
 
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.BottomSheetDialog;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +30,13 @@ public class IssueSummaryActivity extends AppCompatActivity implements  IssueLis
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back_black_24dp);
+
+
+        //Show Bottom Sheet options
         View view = getLayoutInflater().inflate(R.layout.fragment_issue_list_dialog, null);
 
         optionsList = new ArrayList<>();
@@ -39,4 +52,33 @@ public class IssueSummaryActivity extends AppCompatActivity implements  IssueLis
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.estimate_menu, menu);
+
+        MenuItem action_refresh = menu.findItem(R.id.action_refresh);
+        Drawable drawable = action_refresh.getIcon();
+        if (drawable != null) {
+            drawable.mutate();
+            drawable.setColorFilter(getResources().getColor(R.color.colorPrimaryDark), PorterDuff.Mode.SRC_ATOP);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // action with ID action_refresh was selected
+            case R.id.action_refresh:
+                Toast.makeText(this, "Refreshing..", Toast.LENGTH_SHORT)
+                        .show();
+                break;
+
+            default:
+                break;
+        }
+
+        return true;
+    }
 }
